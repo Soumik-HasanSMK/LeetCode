d3c8a051c6b023ce062610585442348288dcdd89
Soumik Hasan <144214624+Soumik-HasanSMK@users.noreply.github.com>
2024-10-13 00:17:10 +0600

    Time: 2 ms (87.85%), Space: 11.8 MB (64.64%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.end());
+        nums.erase(unique(nums.begin(),nums.end()),nums.end());
+        if(nums.size()<3)
+            return nums[0];
+        else
+            return nums[2];
+    }
+};
